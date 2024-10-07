f8b82fd2b19cac752a77a2cc6f7ce14e96dadd66
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2024-10-07 22:40:30 +0530

    Add files via upload

---
+++ Addition of Two Numbers.rb
@@ -0,0 +1,5 @@
+# Program to add two numbers
+num1 = 10
+num2 = 20
+sum = num1 + num2
+puts "The sum of #{num1} and #{num2} is #{sum}"
---
+++ Power of a Number.rb
@@ -0,0 +1,5 @@
+# Program to calculate the power of a number
+base = 2
+exponent = 3
+result = base ** exponent
+puts "#{base} to the power of #{exponent} is #{result}"
